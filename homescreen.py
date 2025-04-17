3470511927f63d9f4cbbb260835f273a15fdb7dc
Lusbox <samlusby4@gmail.com>
2025-04-17 13:31:29 +0100

    basic search function, test inventory

--- homescreen.py
+++ homescreen.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from PIL import Image, ImageTk
+from test_inventory import *
 
 root = tk.Tk()
 root.title("Granblue Fantasy Search")
@@ -49,28 +50,54 @@ def homescreen():
         for widget in frm.winfo_children():
             widget.destroy()
 
-        result = tk.Label(frm, text=f"Number of {search_term.get()} in inventory : 4",
+        weapon_count = 0
+        weapon = search_term.get().lower()
+        weapon_pic = ""
+        result_pic = ""
+        
+
+        for item in gbf_invent:
+            if item["name"] == weapon:
+                weapon_count += 1
+                weapon_pic = item["image"]
+                lyria = Image.open("assets/lyria_jump.png")
+                result_pic = lyria.resize((lyria.width//2, lyria.height//2))
+
+            
+        if weapon_pic:
+            weapon_image = Image.open(weapon_pic)
+            weapon_image = weapon_image.resize((weapon_image.width//2, weapon_image.height//2))
+            weapon_photo = ImageTk.PhotoImage(weapon_image)
+            weapon = tk.Label(frm, image=weapon_photo, bg="black")
+
+            lyria_result = ImageTk.PhotoImage(result_pic)
+            lyria = tk.Label(frm, image=lyria_result, bg="black")
+
+            frm.weapon = weapon_photo
+            frm.lyria = lyria_result
+
+            
+            weapon.place(x=50, rely=0.5, anchor='w')
+            lyria.place(x=670, rely=0.5, anchor='e')
+
+        else:
+            siero_pic = Image.open("assets/Sierokarte_NPC.webp")
+            result_pic = siero_pic.resize((int(siero_pic.width//1.75), int(siero_pic.height//1.75)))
+            siero_result = ImageTk.PhotoImage(result_pic)
+            siero = tk.Label(frm, image=siero_result, bg="black")
+            siero.place(relx=0.5, rely=0.5, anchor='center')
+
+            frm.siero = siero_result
+
+
+        result = tk.Label(frm, text=f"Number of {search_term.get()} in inventory : {weapon_count}",
                         font=("Arial", 14),
                         fg="white",
                         bg="black")
-
-        weapon_image = Image.open("assets/Weapon_b_1040906400.png")
-        weapon_image = weapon_image.resize((weapon_image.width//2, weapon_image.height//2))
-        weapon_photo = ImageTk.PhotoImage(weapon_image)
-        weapon = tk.Label(frm, image=weapon_photo, bg="black")
-
-        lyria_result = ImageTk.PhotoImage(Image.open("assets/lyria_heat.png"))
-        lyria = tk.Label(frm, image=lyria_result, bg="black")
-
-        back_button = tk.Button(frm, text="Back", command=homescreen)
-
-        frm.weapon = weapon_photo
-        frm.lyria = lyria_result
         
-        result.place(relx=0.5, y= 50, anchor='center')
-        weapon.place(x=50, rely=0.5, anchor='w')
+        back_button = tk.Button(frm, text="Back", command=homescreen)
         back_button.place(relx=0.5, y=420, anchor='center')
-        lyria.place(x=670, rely=0.5, anchor='e')
+        result.place(relx=0.5, y= 50, anchor='center')
 
 
     
